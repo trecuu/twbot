82a366f573e320d08e50b867e82d3def2ca4aabc
trecuu <ricardoperazza@hotmail.com>
2019-04-01 20:38:23 -0300

    Update twbot.py

--- twbot.py
+++ twbot.py
@@ -7,10 +7,11 @@ class twbot:
 		self.channel = channel
 		self.port = 6667
 		self.host = 'irc.twitch.tv'
-		self.socket = ''
+		self.socket = None
 
 	def Send_message(self, message):
-		self.socket.send(("PRIVMSG "+ self.channel +" :" + message + "\r\n").encode())
+		#self.socket.send(("PRIVMSG "+ self.channel +" :" + message + "\r\n").encode())
+		print("PRIVMSG "+ self.channel +" :" + message + "\r\n")
 
 	def Banplayer(self, username, time=None):
 		if time is None:
@@ -18,43 +19,45 @@ class twbot:
 			self.Send_message("User : "+username+" foi banido do canal.")
 		else:
 			self.Send_message(".timeout " + username + " " + str(time))
-			self.Send_message("User : "+username+" foi banido do canal por "+str(time)+" segundos.")	
+			self.Send_message("User : "+username+" foi banido do canal por "+str(time)+" segundos.")
 	
 	def Alo(self, username):
-			self.Send_message(username + ", VAI TUMA NO CU")
+			self.Send_message(username + ", ALOOOOOOOOO")
 
 
 	def Listen(self):
-		s = socket
-		readbuffer = readbuffer + s.recv(1024).decode()
-		temp = readbuffer.split("\n")
-		readbuffer = temp.pop()
-		for line in temp:
-			if (line.split()[0] == "PING"):
-				print (line)
-				s.send(("PONG "+ line.split()[1] + "\r\n").encode())
-			else:
-				parts = line.split(":")
-				if "QUIT" not in parts[1] and "JOIN" not in parts[1] and "PART" not in parts[1]:
-					try:
-						message = parts[2][:len(parts[2]) - 1]
-					except:
-						message = ""
-					usernamesplit = parts[1].split("!")
-					username = usernamesplit[0]
-					if MODT:
-						print (username + ": " + message)
-						if message.lower() == "alo":
-							self.Alo(username)
-						if username == "macotv":
-							if message.split()[0] == "!ban123":
-								if len(message.split()) == 3:
-									self.Banplayer(message.split()[-2],message.split()[-1])
-								if len(message.split()) == 2:
-									self.Banplayer(message.split()[-2])
-					for l in parts:
-						if "End of /NAMES list" in l:
-							MODT = True
+		if self.socket != None:
+			s = self.socket
+			readbuffer = readbuffer + s.recv(1024).decode()
+			temp = readbuffer.split("\n")
+			readbuffer = temp.pop()
+			for line in temp:
+				if (line.split()[0] == "PING"):
+					print (line)
+					s.send(("PONG "+ line.split()[1] + "\r\n").encode())
+				else:
+					parts = line.split(":")
+					if "QUIT" not in parts[1] and "JOIN" not in parts[1] and "PART" not in parts[1]:
+						try:
+							message = parts[2][:len(parts[2]) - 1]
+						except:
+							message = ""
+						username = parts[1].split("!")[0]
+						if MODT:
+							print (username + ": " + message)
+							if message.lower() == "alo":
+								self.Alo(username)
+							if username == "macotv":
+								if message.split()[0] == "!ban123":
+									if len(message.split()) == 3:
+										self.Banplayer(message.split()[-2],message.split()[-1])
+									if len(message.split()) == 2:
+										self.Banplayer(message.split()[-2])
+						for l in parts:
+							if "End of /NAMES list" in l:
+								MODT = True
+		else:
+			print("Bot not CONNECTED! (try Connect())")	
 
 
 
@@ -70,6 +73,8 @@ class twbot:
 bot = twbot("trecuu","oauth:SEGREDINHO","#macotv")
 bot.name = "alo"
 print(bot.host)
+bot.Banplayer("pinga123")
+bot.Listen()
 
 
 #testing

25980c468226b8b560bce4a1a6e2b7003b2e101f
trecuu <ricardoperazza@hotmail.com>
2019-04-01 23:48:03 -0300

    Update twbot.py

--- twbot.py
+++ twbot.py
@@ -1,4 +1,5 @@
 import socket
+import sys
 
 class twbot:
 	def __init__(self, name, passwd, channel):
@@ -8,10 +9,10 @@ class twbot:
 		self.port = 6667
 		self.host = 'irc.twitch.tv'
 		self.socket = None
+		self.MODT = False
 
 	def Send_message(self, message):
-		#self.socket.send(("PRIVMSG "+ self.channel +" :" + message + "\r\n").encode())
-		print("PRIVMSG "+ self.channel +" :" + message + "\r\n")
+		self.socket.send(("PRIVMSG #"+ self.channel +" :" + message + "\r\n").encode())
 
 	def Banplayer(self, username, time=None):
 		if time is None:
@@ -24,16 +25,16 @@ class twbot:
 	def Alo(self, username):
 			self.Send_message(username + ", ALOOOOOOOOO")
 
-
 	def Listen(self):
+		readbuffer = ''
 		if self.socket != None:
 			s = self.socket
 			readbuffer = readbuffer + s.recv(1024).decode()
 			temp = readbuffer.split("\n")
 			readbuffer = temp.pop()
 			for line in temp:
+				print(line)
 				if (line.split()[0] == "PING"):
-					print (line)
 					s.send(("PONG "+ line.split()[1] + "\r\n").encode())
 				else:
 					parts = line.split(":")
@@ -43,38 +44,48 @@ class twbot:
 						except:
 							message = ""
 						username = parts[1].split("!")[0]
-						if MODT:
+						if self.MODT:
 							print (username + ": " + message)
 							if message.lower() == "alo":
 								self.Alo(username)
-							if username == "macotv":
+							
+							if (username == "mistertag") or (username == "trecuu"):
 								if message.split()[0] == "!ban123":
 									if len(message.split()) == 3:
 										self.Banplayer(message.split()[-2],message.split()[-1])
 									if len(message.split()) == 2:
 										self.Banplayer(message.split()[-2])
+								elif message.split()[0] == "!botquit":
+									self.Send_message(" Bot vazando! Flw vlw!")
+									self.socket.close()
+									print("Bot Disconected!!!")
+									sys.exit()
+
 						for l in parts:
 							if "End of /NAMES list" in l:
-								MODT = True
+								self.MODT = True
 		else:
 			print("Bot not CONNECTED! (try Connect())")	
-
-
 
 	def Connect(self):
 		s = socket.socket()
 		s.connect((self.host, self.port))
 		s.send(("PASS " + self.passwd  + "\r\n").encode())
 		s.send(("NICK " + self.name    + "\r\n").encode())
-		s.send(("JOIN " + self.channel + "\r\n").encode())
+		s.send(("JOIN #" + self.channel + "\r\n").encode())
 		self.socket = s
 
-#testing
-bot = twbot("trecuu","oauth:SEGREDINHO","#macotv")
-bot.name = "alo"
-print(bot.host)
-bot.Banplayer("pinga123")
-bot.Listen()
 
+
+#testing
+oauth = open("oauth.txt","r")
+oauth = oauth.read()
+
+bot = twbot("trecuu",oauth,"trecuu")
+
+bot.Connect()
+
+while True:
+	bot.Listen()
 
 #testing
